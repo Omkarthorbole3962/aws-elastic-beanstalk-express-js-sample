4c840e360576c8456ce657263eaedcb170739524
omkarthorbole <omkarthorbole15701@gmail.com>
2024-06-23 16:38:06 +0000

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello FOlks! Omkar Thorbole on this side'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
